3861d5ac51fa0cb483d2bf6a92cebf6c28295cc1
Semyon <saimonpanov@gmail.com>
2017-03-20 12:59:51 +0400

    Update script.js

--- recursion/script.js
+++ recursion/script.js
@@ -1,58 +1,58 @@
-
 const response = ["HI", ["That's", "My", ['First', 'JavaScript code']], "!!!", "=)", ['Thanks for', ['Your attention!']]];
 
 function createArray() {
 
-function createList(arr, ul) {
+    function createList(arr, ul) {
 
-for (let i = 0; i < arr.length; i++) {
+        for (let i = 0; i < arr.length; i++) {
 
-  if (arr[i] instanceof Array) {
+            if (arr[i] instanceof Array) {
 
-    var btn = document.createElement('input');
-    btn.className = 'toggle';
-    btn.setAttribute('identification', 'inner_' + id);
-    btn.type = 'button';
-    btn.value = 'Open';
-    ul.appendChild(btn);
+                let btn = document.createElement('input');
+                btn.className = 'toggle';
+                btn.setAttribute('identification', 'inner_' + id);
+                btn.type = 'button';
+                btn.value = 'Open';
+                ul.appendChild(btn);
 
-    const innerUl = document.createElement('ul');
-    innerUl.id = 'inner_' + id;
-    id = ++id;
-    createList(arr[i], innerUl);
-    ul.appendChild(innerUl);
+                const innerUl = document.createElement('ul');
+                innerUl.id = 'inner_' + id;
+                id = ++id;
+                createList(arr[i], innerUl);
+                ul.appendChild(innerUl);
 
-  } else {
-    const li = document.createElement('li');
-    li.appendChild(document.createTextNode(arr[i]));
-    ul.appendChild(li);
-  }
-}
-}
+            } else {
+                const li = document.createElement('li');
+                li.appendChild(document.createTextNode(arr[i]));
+                ul.appendChild(li);
+            }
+        }
+    }
 
-var id = 1;
-const ul = document.createElement('ul');
-ul.id = "parent_ul";
-createList(response, ul);
+    let id = 1;
+    const ul = document.createElement('ul');
+    ul.id = "parent_ul";
+    createList(response, ul);
 
-document.body.appendChild(ul);
-var x = document.getElementsByClassName('toggle');
+    document.body.appendChild(ul);
+    let x = document.getElementsByClassName('toggle');
 
-function showHide() {
+    function showHide() {
 
-var element_attribute = this.getAttribute('identification');
-var element_id = document.getElementById(element_attribute);
+        let element_attribute = this.getAttribute('identification');
+        let element_id = document.getElementById(element_attribute);
 
-if (element_id.style.display == "none") {
-  element_id.style.display = "block";
-} else {
-  element_id.style.display = "none";
-}
+        if (element_id.style.display == "none") {
+            element_id.style.display = "block";
+        } else {
+            element_id.style.display = "none";
+        }
+    }
 
-}
-for (j = 0; j < x.length; j++) {
-x[j].addEventListener('click', showHide);
-}
+    for (let j = 0; j < x.length; j++) {
+        x[j].addEventListener('click', showHide);
+    }
 }
 
 createArray();
+
